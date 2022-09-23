0cae296bb2b37ad9b8d957925ed1a70f6cb50929
Jan Arend <janarendvw@gmail.com>
2022-09-23 09:27:49 +0200

    Added multiple page.tsx files

--- App/src/App.tsx
+++ App/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./layout/Navbar";
+import AboutMePage from "./pages/about/AboutMePage";
 import HomePage from "./pages/home/HomePage";
 
 function App() {
@@ -9,11 +10,12 @@ function App() {
 
   return (
     <div className="grid grid-cols-12 gap-4 w-full min-h-screen grid-rows-6">
-    <Navbar/>
     <BrowserRouter>
+    <Navbar/>
       <Routes>
         <Route path="*" element={<HomePage />} />
-        <Route path="/about-me" element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/about-me" element={<AboutMePage />} />
         <Route path="/projects" element={<HomePage />} />
         <Route path="/contact" element={<HomePage />} />
       </Routes>
---
+++ App/src/pages/contact/ContactPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function ContactPage({}: Props) {
+  return (
+    <div>ContactPage</div>
+  )
+}
