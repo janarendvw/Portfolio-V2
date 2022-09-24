53d753ebfabd5308038ab83ff237d5d9e1a6bf47
Jan Arend <janarendvw@gmail.com>
2022-09-24 16:02:21 +0200

    adjusted button click anminations

--- App/tailwind.config.cjs
+++ App/tailwind.config.cjs
@@ -24,7 +24,7 @@ module.exports = {
           "--animation-btn": "0.25s", // duration of animation when you click on button
           "--animation-input": "0.2s", // duration of animation for inputs like checkbox, toggle, radio, etc
           "--btn-text-case": "capitalize", // set default text transform for buttons
-          "--btn-focus-scale": "1", // scale transform of button when you focus on it
+          "--btn-focus-scale": "0.8", // scale transform of button when you focus on it
           "--border-btn": "1px", // border width of buttons
           "--tab-border": "1px", // border width of tabs
           "--tab-radius": "0.5rem", // border radius of tabs
