d956365328439734e5c65d6425b41ceb6e015c04
Jan Arend <janarendvw@gmail.com>
2022-09-24 18:31:58 +0200

    added project links

--- App/src/pages/projects/ProjectsPage.tsx
+++ App/src/pages/projects/ProjectsPage.tsx
@@ -1,118 +1,139 @@
 import React, { useEffect } from "react";
-import { XCircle } from "react-feather";
-import './projectPage.scss'
+import { ExternalLink, GitPullRequest, XCircle } from "react-feather";
+import "./projectPage.scss";
 type Props = {};
 const projects = [
   {
     id: 1,
     title: "Project 1",
-    description: "This is project 1",
+    description:
+      "this text contains 50 words: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies lacinia, nunc nisl ultricies nisl, nec ultricies nunc nisl eget nunc. Donec auctor, nisl eget ultricies lacinia, nunc nisl ultricies nisl, nec ultricies nunc nisl eget nunc.",
     image: "https://picsum.photos/600/300",
   },
   {
     id: 2,
     title: "Project 2",
-    description: "This is project 2",
+    description:
+      "this text contains 50 words: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies lacinia, nunc nisl ultricies nisl, nec ultricies nunc nisl eget nunc. Donec auctor, nisl eget ultricies lacinia, nunc nisl ultricies nisl, nec ultricies nunc nisl eget nunc.",
+   
     image: "https://picsum.photos/200/300",
   },
   {
     id: 3,
     title: "Project 3",
-    description: "This is project 3",
-    image: "https://picsum.photos/300/300",
+    description:
+    "this text contains 50 words: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies lacinia, nunc nisl ultricies nisl, nec ultricies nunc nisl eget nunc. Donec auctor, nisl eget ultricies lacinia, nunc nisl ultricies nisl, nec ultricies nunc nisl eget nunc.",
+     image: "https://picsum.photos/300/300",
   },
   {
     id: 4,
     title: "Project 4",
-    description: "This is project 4",
-    image: "https://picsum.photos/800/300",
+    description:
+    "this text contains 50 words: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies lacinia, nunc nisl ultricies nisl, nec ultricies nunc nisl eget nunc. Donec auctor, nisl eget ultricies lacinia, nunc nisl ultricies nisl, nec ultricies nunc nisl eget nunc.",
+     image: "https://picsum.photos/800/300",
   },
   {
     id: 5,
     title: "Project 5",
-    description: "This is project 5",
-    image: "https://picsum.photos/500/300",
+    description:
+    "this text contains 50 words: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies lacinia, nunc nisl ultricies nisl, nec ultricies nunc nisl eget nunc. Donec auctor, nisl eget ultricies lacinia, nunc nisl ultricies nisl, nec ultricies nunc nisl eget nunc.",
+     image: "https://picsum.photos/500/300",
   },
 ];
 export default function ProjectsPage({}: Props) {
+  useEffect(() => {
+    const slider: any = document.querySelector("#projects-scroller");
+    let isDown = false;
+    let startX: any;
+    let scrollLeft: any;
 
-useEffect(() => {
-  const slider:any = document.querySelector("#projects-scroller");
-  let isDown = false;
-  let startX: any;
-  let scrollLeft: any;
-  
-  slider?.addEventListener("mousedown", (e: { pageX: number; }) => {
-    isDown = true;
-    slider.classList.add("active");
-    startX = e.pageX - slider.offsetLeft;
-    scrollLeft = slider.scrollLeft;
-  });
-  slider?.addEventListener("mouseleave", () => {
-    isDown = false;
-    slider.classList.remove("active");
-  });
-  slider?.addEventListener("mouseup", () => {
-    isDown = false;
-    slider.classList.remove("active");
-  });
-  slider?.addEventListener("mousemove", (e: { preventDefault: () => void; pageX: number; }) => {
-    if (!isDown) return;
-    e.preventDefault();
-    const x = e.pageX - slider.offsetLeft;
-    const walk = x - startX;
-    slider.scrollLeft = scrollLeft - walk;
-  });
-}, [])
-
+    slider?.addEventListener("mousedown", (e: { pageX: number }) => {
+      isDown = true;
+      slider.classList.add("active");
+      startX = e.pageX - slider.offsetLeft;
+      scrollLeft = slider.scrollLeft;
+    });
+    slider?.addEventListener("mouseleave", () => {
+      isDown = false;
+      slider.classList.remove("active");
+    });
+    slider?.addEventListener("mouseup", () => {
+      isDown = false;
+      slider.classList.remove("active");
+    });
+    slider?.addEventListener(
+      "mousemove",
+      (e: { preventDefault: () => void; pageX: number }) => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - slider.offsetLeft;
+        const walk = x - startX;
+        slider.scrollLeft = scrollLeft - walk;
+      }
+    );
+  }, []);
 
   return (
     <div className="flex justify-between col-start-2 row-start-2 row-end-2 col-end-12">
       <div id="projects-section" className="w-2/3">
         <div className="flex flex-col lg:flex-row md:justify-between items-start">
-        <div className="text-4xl font-bold">My projects</div>
-        <div
-          id="projects-skill-filter"
-          className="items-center gap-4 flex hover:opacity-100 cursor-pointer"
-        >
-          <span className="opacity-60 font-bold">Filter by skill:</span>
-          <span className="gap-4 flex p-4 rounded-md">
-            <span className="badge badge-outline badge-warning opacity-50 hover:opacity-100 cursor-pointer">
-              html
+          <div className="text-4xl font-bold">My projects</div>
+          <div
+            id="projects-skill-filter"
+            className="items-center gap-4 flex hover:opacity-100 cursor-pointer"
+          >
+            <span className="opacity-60 font-bold">Filter by skill:</span>
+            <span className="gap-4 flex p-4 rounded-md">
+              <span className="badge badge-outline badge-warning opacity-50 hover:opacity-100 cursor-pointer">
+                html
+              </span>
+              <span className="badge badge-outline badge-info opacity-50 hover:opacity-100 cursor-pointer">
+                Wordpress
+              </span>
+              <span className="badge badge-primary cursor-pointer">
+                React js
+              </span>
+              <span className="badge badge-outline badge-success opacity-50 hover:opacity-100 cursor-pointer">
+                Vue js
+              </span>
             </span>
-            <span className="badge badge-outline badge-info opacity-50 hover:opacity-100 cursor-pointer">
-              Wordpress
-            </span>
-            <span className="badge badge-primary cursor-pointer">React js</span>
-            <span className="badge badge-outline badge-success opacity-50 hover:opacity-100 cursor-pointer">
-              Vue js
-            </span>
-          </span>
-        </div>
+          </div>
         </div>
         <div id="projects-container">
-        <div
-          id="projects-scroller"
-          className="grid overflow-auto grid-flow-row md:grid-flow-col gap-4 p-4 cursor-grab"
-        >
-          {projects.map((project, index) => {
-            return (
+          <div
+            id="projects-scroller"
+            className="grid overflow-auto grid-flow-row md:grid-flow-col gap-4 p-4 cursor-grab"
+          >
+            {projects.map((project, index) => {
+              return (
                 <div
-                  style={{ background: `url(${project.image})`, backgroundSize: "cover", backgroundPosition: "center", "--animation-order": index } as React.CSSProperties }
-                  id="project" className="snap-center h-96 w-96 rounded-lg shadow-lg flex flex-col justify-between overflow-hidden hover:scale-[1.03] duration-100"
+                  style={
+                    {
+                      background: `url(${project.image})`,
+                      backgroundSize: "cover",
+                      backgroundPosition: "center",
+                      "--animation-order": index,
+                    } as React.CSSProperties
+                  }
+                  id="project"
+                  className="snap-center h-96 w-96 rounded-lg shadow-lg flex flex-col justify-between overflow-hidden duration-100"
                 >
-                  <div className="bg-black/70 hover:bg-black/20 duration-200 h-full flex flex-col justify-between p-4">
-                  <div className="flex flex-col n">
-                    <div className="text-2xl font-bold">{project.title}</div>
-                    <div className="text-xl opacity-80">
-                      {project.description}
+                  <div className="bg-black/70 hover:bg-black/20 duration-200 h-full flex flex-col justify-between">
+                    <div id="project-text" className="p-4">
+                      <div className="text-2xl font-bold">{project.title}</div>
+                      <div className="text-xl opacity-80">
+                        {project.description}
+                      </div>
+                      </div>
+                      <span className="flex justify-between bg-black/60 px-2 items-center pb-2">
+                      <a className="btn btn-sm btn-primary mt-4 w-max flex gap-2" href="#"><ExternalLink size={16}/> visit</a>
+                      <a className="btn btn-sm btn-outline btn-secondary mt-4 w-max flex gap-2" href="#"><GitPullRequest size={16}/> Source Code</a>
+                      </span>
                     </div>
                   </div>
-                  </div>
-                </div>
-            );
-          })}
-        </div>
+              );
+            })}
+          </div>
         </div>
       </div>
       <div id="projects-contact" className="w-1/4">
