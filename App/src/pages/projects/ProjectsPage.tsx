da382b40e64e9fe9969e427917c6026568a949fa
Jan Arend <janarendvw@gmail.com>
2022-09-24 11:14:20 +0200

    added projects layout

--- App/src/pages/projects/ProjectsPage.tsx
+++ App/src/pages/projects/ProjectsPage.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 type Props = {}
 
 export default function ProjectsPage({}: Props) {
-  return (
-    <div>ProjectsPage</div>
+  return (<>
+    <div className='bg-white/10 row-start-2 col-start-2 col-end-9'>
+      <div id='projects' className='flex flex-col items-center'></div>
+    </div>
+    <div className='bg-white/10 row-start-2 col-start-9 col-end-12'> </div>
+    </>
   )
 }
