d0eee70ba166f4497511a0706ae6b74e96167651
Jan Arend <janarendvw@gmail.com>
2022-09-29 11:18:32 +0200

    added animations to contactpage

--- App/src/pages/contact/ContactPage.tsx
+++ App/src/pages/contact/ContactPage.tsx
@@ -6,13 +6,13 @@ type Props = {}
 export default function ContactPage({}: Props) {
   return (
     <div className='flex justify-between col-start-2 col-span-10 row-start-2'>
-      <div id="contact-hero-left" className="w-1/3">
+      <div id="contact-hero-left" className="w-1/3 slide-left">
         <div className="text-6xl">Let’s get in touch!</div>
         <div className="text-xl opacity-80 mt-4">
           I am always open to new opportunities and collaborations. Feel free to contact me via email or LinkedIn.
           </div>
           </div>
-          <div id="contact-hero-right" className="w-96">
+          <div id="contact-hero-right" className="w-96 slide-up">
             <ContactForm/>
             </div>
     </div>
